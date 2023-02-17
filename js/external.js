1a3dd0da16b97c64048dbb3c1fda6aee071f1049
Xavier Francis <james.x.francis9@gmail.com>
2023-02-17 14:22:17 -0600

    javascript with html exercises completed

--- js/external.js
+++ js/external.js
@@ -1,10 +1,33 @@
 "use strict";
 
-console.log("This is an external js file");
+console.log("Hello from external javascript.");
 
-alert("This is an alert.");
+alert("Welcome to my Website!");
 
-confirm("Are you sure you want to enter this webpage?");
+let favColor = prompt("What is your favorite color?");
+alert(favColor + ", is my favorite color too!");
 
-let userInput = prompt("Please enter your name: ");
-alert("Welcome, " + userInput);
+let dailyRental = Number(prompt("What is the cost to rent a movie for a day?"));
+let littleMermaid = Number(prompt("How many days did you rent Little Mermaid?"));
+let brotherBear = Number(prompt("How many days did you rent Brother Bear?"));
+let hercules = Number(prompt("How many days did you rent Hercules?"));
+alert("Your rental cost is a total of $" + (littleMermaid + brotherBear + hercules) * dailyRental);
+
+let googlePay = Number(prompt("How much does Google pay you per hour?"));
+let amazonPay = Number(prompt("How much does Amazon pay you per hour?"));
+let facebookPay = Number(prompt("How much does Facebook pay you per hour?"));
+let googleHours = Number(prompt("How many hours did you work at Google?"));
+let amazonHours = Number(prompt("How many hours did you work at Amazon?"));
+let facebookHours = Number(prompt("How many hours did you work at Facebook?"));
+alert("Your total check should be $" + ((googlePay * googleHours) + (amazonPay * amazonHours) + (facebookPay * facebookHours)));
+//
+let classStatus = confirm("Select 'OK' if the class is open");
+let scheduleStatus = confirm("Select 'OK' if the your schedule is open");
+let canEnroll = classStatus.toString(); + scheduleStatus.toString();
+alert("You are able to enroll into a new class: " + canEnroll);
+
+let items = confirm("Select 'OK' if 2 or more items are being purchased");
+let offer = confirm("Select 'OK' if the offer has not expired");
+let membership =confirm("Select 'OK' if the customer is a Premium Member");
+let offerAvailability = membership || (items && offer);
+alert("Offer availability: " + offerAvailability);
