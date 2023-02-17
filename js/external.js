601f916ecf080aed6fa2555660d34cf6c5e73cf7
Xavier Francis <james.x.francis9@gmail.com>
2023-02-17 12:24:19 -0600

    js with html lecture notes

---
+++ js/external.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log("This is an external js file");
+
+alert("This is an alert.");
+
+confirm("Are you sure you want to enter this webpage?");
+
+let userInput = prompt("Please enter your name: ");
+alert("Welcome, " + userInput);
